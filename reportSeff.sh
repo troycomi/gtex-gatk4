e121986545099abeb3c8b1ee4f9341c275023c84
Troy Comi <troycomi@gmail.com>
2019-02-12 11:14:54 -0500

    Report seff improvements
    Moved formatting into script with awk, made directory optional, taking
    current directory as default.  Find is also more specific

--- reportSeff.sh
+++ reportSeff.sh
@@ -5,24 +5,28 @@
 # usage reportSeff.sh OUT_DIR
 # assumes out files are named as NAME_JOB_ARRAY.out, no _ in NAME
 
-set -euo pipefail
-
 if [ "$#" -ne 1 ] || [ -z "$1" ] || [ ! -d $1 ]; then
-    echo "Error: expected directory argument"
-    echo "Usage: $0 DIRECTORY"
+    DIR=$(pwd)
+else
+    DIR=$1
 fi
 
-find "$1" -type f -print0 | sort -z | \
+printf "%12s%6s%12s\t%-15s%-10s%-15s%s\n" "" Job "" State CPU Time Memory
+find "$DIR" -maxdepth 1 -type f -regex '.*_[0-9][0-9]*$' -print0 | sort -z | \
     while IFS= read -r -d '' FILE; do
         JOB=$(basename $FILE)
         JOB=${JOB##*_}
 
         #test if job is still running
         [[ `squeue -j $JOB 2> /dev/null | grep $JOB` ]] \
-            && echo -e "$JOB is still running" && continue 
+            && continue 
 
         #append seff
-        echo $(basename $FILE)
-        seff $JOB 
-        echo ''
+        printf "%30s\t" $(basename $FILE)
+        awk '
+        /State:/{printf "%-15s", $2}
+        /CPU Eff/{printf "%-10s", $3}
+        /Job Wall/{printf "%-15s", $4}
+        /Memory Eff/{printf "%s %s %s %s\n", $3, $4, $5, $6}
+        ' <(seff $JOB)
     done
