d26dd40fe0f35c0ee007b999f3f5fa4fe8dc6093
Troy Comi <troycomi@gmail.com>
2019-12-02 08:56:44 -0500

    Add attempt to callable
    Added the attempt variable to resource estimation on call_depth

--- run_interactive.sh
+++ run_interactive.sh
@@ -4,8 +4,14 @@
 
 module load anaconda3
 conda activate gtex-gatk4
-PATH=$PATH:/usr/licensed/anaconda3/2019.3/bin
 
+base_dir=/projects/AKEY/akey_vol2/serenatu/data/callable_bases/bed/
+samples=(LP6005441-DNA_B03 LP6005441-DNA_G10 LP6005592-DNA_A02 LP6005442-DNA_E10 LP6005677-DNA_G01 LP6005443-DNA_B04 LP6005442-DNA_H01 LP6005442-DNA_G09 SS6004477 LP6005592-DNA_D03 LP6005443-DNA_C03)
+paths=()
+for s in ${samples[@]}; do
+    paths+="$base_dir$s/$s.summary.gz "
+done
 snakemake --use-conda \
     --use-singularity -w 60 \
-    --resources fastq_instances=5
+    -j 5 \
+    ${paths[@]}
